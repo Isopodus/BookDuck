9b4d7a090a0f9f445350d7655840506201bdfbff
Eugene Turchik <eturchick@gmail.com>
2022-01-22 17:54:17 +0200

    TTS test

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import Duck from "./src/Duck";
+import Tts from "react-native-tts";
 
 import { StackNavigator } from "./src/navigation/StackNavigator";
 
@@ -8,11 +9,20 @@ import { theme } from "./src/ui";
 
 export const App = () => {
   useEffect(() => {
-    const duck = new Duck();
-    duck.proceedDialog("I'm so happy! Got my new car, so I can travel across the whole country!").then(res => {
-      console.log(res);
-      // duck.proceedDialog("I want to get a better salary!").then(res => console.log(res));
+    // Tts.voices().then(voices => voices.forEach(voice => voice.name.startsWith("en-us-x") && console.log(voice.name)));
+    Tts.getInitStatus().then(() => {
+      Tts.setDefaultLanguage("en-US");
+      Tts.setDefaultVoice("en-us-x-tpd-network");
+      Tts.setDefaultPitch(1.5);
+      Tts.speak("Hello there! I'm the BookDuck");
     });
+
+    // const duck = new Duck();
+    // duck.proceedDialog("I'm so happy! Got my new car, so I can travel across the whole country!").then(res => {
+    //   console.log(res);
+
+    //   // duck.proceedDialog("I want to get a better salary!").then(res => console.log(res));
+    // });
   });
 
   return (
